fc9b50aaee2856ee05905b1ff5fdfbdde0d983ea
EnzoLouni <enzo.louni@cpe.fr>
2023-04-18 14:18:27 +0200

    Strategy Pattern WIP

---
+++ src/main/java/model/strategy/movementStrategy/PionCavalierStrategy.java
@@ -0,0 +1,10 @@
+package model.strategy.movementStrategy;
+
+import shared.ActionType;
+
+public class PionCavalierStrategy implements MovementStrategy{
+    @Override
+    public boolean isMoveOk(int xInit, int yInit, int xFinal, int yFinal, boolean hasMoved, ActionType actionType) {
+        return false;
+    }
+}
